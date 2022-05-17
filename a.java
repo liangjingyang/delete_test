d166d39fc251ba8d8a36683e18906f92e0912514
Jingyang Liang <jingyang@merico.dev>
2022-05-17 18:06:15 +0800

    add a.java

---
+++ a.java
@@ -0,0 +1,14 @@
+package com.example;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * @author who
+ * @date 2022-05-17 11:27
+public class CodeTester2 {
+    public void add(){
+        List<String> list = new ArrayList<>();
+        list.add("d");
+        list.add("e");
+        list.add("f");
+    }
+}
