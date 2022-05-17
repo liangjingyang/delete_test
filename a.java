7c986bbfdc3efda5e3edb66c2003053f47a0d848
Jingyang Liang <jingyang@merico.dev>
2022-05-17 18:06:45 +0800

    update a.java

--- a.java
+++ a.java
@@ -6,9 +6,6 @@ import java.util.List;
  * @date 2022-05-17 11:27
 public class CodeTester2 {
     public void add(){
-        List<String> list = new ArrayList<>();
-        list.add("d");
-        list.add("e");
-        list.add("f");
+
     }
 }
